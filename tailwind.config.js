e98700c7b9cd1f658e4621c9c1751ac8ce399fdc
YOOJS1205 <junsang8921@gmail.com>
2025-02-17 11:17:41 +0000

    design token custom (#62)
    * tailwind 변수 값 세팅
    
    * 자동완성을 위해tailwind.config.js 에 클래서 적용
    
    * edit variable name ex) ts -> title-small
    
    * design color 변경으로 인한 토큰 value 수정
    
    * Update tailwind.config.js
    
    Co-authored-by: 유준상 <89122773+YOOJS1205@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: 유준상 <89122773+YOOJS1205@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+export const content = ['./src/**/*.{html,js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {
+    fontSize: {
+      h1: [
+        '32px',
+        { lineHeight: '1.25', letterSpacing: '-0.02em', fontWeight: 'bold' },
+      ],
+      h2: [
+        '28px',
+        { lineHeight: '1.3', letterSpacing: '-0.02em', fontWeight: 'bold' },
+      ],
+      h3: [
+        '24px',
+        { lineHeight: '1.3', letterSpacing: '-0.015em', fontWeight: 'bold' },
+      ],
+      'title-large': [
+        '22px',
+        { lineHeight: '1.36', letterSpacing: '-0.02em', fontWeight: 'bold' },
+      ],
+      'title-medium': [
+        '20px',
+        { lineHeight: '1.2', letterSpacing: '-0.015em', fontWeight: '600' },
+      ],
+      'title-small': [
+        '18px',
+        { lineHeight: '1.2', letterSpacing: '-0.01em', fontWeight: '600' },
+      ],
+      'body-1-long': ['16px', { lineHeight: '1.3', letterSpacing: '-0.01em' }],
+      'body-1-normal': [
+        '16px',
+        { lineHeight: '1.25', letterSpacing: '-0.01em' },
+      ],
+      'body-2-long': [
+        '14px',
+        { lineHeight: '1.25', letterSpacing: '-0.005em' },
+      ],
+      'body-2-normal': [
+        '14px',
+        { lineHeight: '1.2', letterSpacing: '-0.005em' },
+      ],
+      caption: ['12px', { lineHeight: '1.28', letterSpacing: '0.005em' }],
+      'label-1': [
+        '14px',
+        { lineHeight: '1.25', letterSpacing: '0.005em', fontWeight: 'bold' },
+      ],
+      'label-2': [
+        '12px',
+        { lineHeight: '1.3', letterSpacing: '0.005em', fontWeight: 'bold' },
+      ],
+    },
+  },
+};
+export const plugins = [];
+
