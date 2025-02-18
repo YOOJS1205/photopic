4c60fb33989c96c6756d8e668631d1d011d11668
YOOJS1205 <junsang8921@gmail.com>
2025-02-18 09:00:12 +0000

    디자인 시스템 변경 사항 반영 (#73)

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,35 +23,39 @@ export const theme = {
       ],
       'title-medium': [
         '20px',
-        { lineHeight: '1.2', letterSpacing: '-0.015em', fontWeight: '600' },
+        { lineHeight: '1.25', letterSpacing: '-0.015em', fontWeight: '600' },
       ],
       'title-small': [
         '18px',
         { lineHeight: '1.2', letterSpacing: '-0.01em', fontWeight: '600' },
+      ],
+      'title-x-small': [
+        '16px',
+        { lineHeight: '1.2', letterSpacing: '-0.015em', fontWeight: '600' },
       ],
       'body-1-long': ['16px', { lineHeight: '1.3', letterSpacing: '-0.01em' }],
       'body-1-normal': [
         '16px',
         { lineHeight: '1.25', letterSpacing: '-0.01em' },
       ],
-      'body-2-long': [
-        '14px',
-        { lineHeight: '1.25', letterSpacing: '-0.005em' },
-      ],
+      'body-2-long': ['14px', { lineHeight: '1.25', letterSpacing: '-0.01em' }],
       'body-2-normal': [
         '14px',
-        { lineHeight: '1.2', letterSpacing: '-0.005em' },
+        { lineHeight: '1.2', letterSpacing: '-0.01em' },
       ],
-      caption: ['12px', { lineHeight: '1.28', letterSpacing: '0.005em' }],
-      'label-1': [
+      caption: ['12px', { lineHeight: '1.25', letterSpacing: '-0.02em' }],
+      'label-large': [
+        '16px',
+        { lineHeight: '1.2', letterSpacing: '-0.015em', fontWeight: 'bold' },
+      ],
+      'label-medium': [
         '14px',
-        { lineHeight: '1.25', letterSpacing: '0.005em', fontWeight: 'bold' },
+        { lineHeight: '1.25', letterSpacing: '-0.015em', fontWeight: 'bold' },
       ],
-      'label-2': [
+      'label-small': [
         '12px',
-        { lineHeight: '1.3', letterSpacing: '0.005em', fontWeight: 'bold' },
+        { lineHeight: '1.3', letterSpacing: '-0.015em', fontWeight: 'bold' },
       ],
     },
   },
 };
-export const plugins = [];
