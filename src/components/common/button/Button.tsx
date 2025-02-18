0686fd0013feb645b78eee3a91bdeb8bebdf3dda
YOOJS1205 <junsang8921@gmail.com>
2025-02-18 10:48:09 +0000

    버튼 컴포넌트를 제작한다. (#74)
    * cva, radix page install
    
    * 버튼, 플로팅 버튼 구현
    
    * 사이즈 네이밍 컨벤션 수정
    
    * size 고정으로 인한 불필요 padding 값 삭제, 디자인 일부 수정, buttonType optional 수정

---
+++ src/components/common/button/Button.tsx
@@ -0,0 +1,61 @@
+import { Slot } from '@radix-ui/react-slot';
+import { cva, VariantProps } from 'class-variance-authority';
+import React from 'react';
+import { cn } from '@/utils/cn';
+
+const buttonVariants = cva('flex items-center justify-center cursor-pointer', {
+  variants: {
+    variant: {
+      solid: '',
+      outline: 'border bg-gray-100',
+    },
+    size: {
+      small: 'w-[157px] h-[40px]  rounded-lg text-label-medium',
+      medium: 'w-[212px] h-[48px]  rounded-xl text-title-small',
+      large: 'w-full h-[63px] rounded-2xl text-title-medium',
+    },
+    solidType: {
+      primary: 'bg-primary-400',
+      secondary: 'bg-primary-600 ',
+      disabled: 'bg-gray-500 text-gray-100',
+    },
+    outlineType: {
+      primary: 'border-primary-400 text-primary-400',
+      secondary: 'border-primary-600 text-primary-600',
+      disabled: 'border-gray-400 text-gray-400',
+    },
+    borderSize: {
+      small: 'border-[1.5px]',
+      medium: 'border-[1.8px]',
+      large: 'border-[2px]',
+    },
+  },
+});
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  buttonType: 'primary' | 'secondary' | 'disabled';
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, size, buttonType, asChild = false, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+
+    const type =
+      variant === 'solid'
+        ? buttonVariants({ solidType: buttonType })
+        : buttonVariants({ outlineType: buttonType, borderSize: size });
+
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size }), type)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  },
+);
---
+++ src/components/common/button/FloatingButton.tsx
@@ -0,0 +1,40 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+import { cn } from '@/utils/cn';
+
+const floatingButtonVariants = cva(
+  'flex items-center justify-center rounded-full cursor-pointer',
+  {
+    variants: {
+      size: {
+        small: 'w-[56px] h-[56px]',
+        large: 'w-[72px] h-[72px]',
+      },
+      buttonType: {
+        primary: 'bg-primary-400',
+        secondary: 'bg-accent-500',
+      },
+    },
+  },
+);
+
+interface FloatingButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof floatingButtonVariants> {
+  children: React.ReactNode;
+}
+
+export const FloatingButton = React.forwardRef<
+  HTMLButtonElement,
+  FloatingButtonProps
+>(({ size, buttonType, children, className, ...props }, ref) => {
+  return (
+    <button
+      className={cn(floatingButtonVariants({ size, buttonType }), className)}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
