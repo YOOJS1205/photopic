90bbac6bc1d0804e77eeac00f937da1cbe3b8c23
YOOJS1205 <junsang8921@gmail.com>
2025-02-28 06:10:09 +0000

    로그인 버튼 클릭 시 redirectURI 절대 경로 설정 (#127)

--- src/components/login/button/LoginButton.tsx
+++ src/components/login/button/LoginButton.tsx
@@ -1,8 +1,7 @@
 import { Button } from '@/components/common/Button/Button';
 
 export default function LoginButton() {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
-
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${window.location.origin}/oauth`;
   const handleLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
