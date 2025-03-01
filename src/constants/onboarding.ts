8de7ec57103c6867a3a0fd221055ac1fc8956132
YOOJS1205 <junsang8921@gmail.com>
2025-03-01 04:51:03 +0000

    온보딩 페이지 구현 (#154)

---
+++ src/constants/onboarding.ts
@@ -0,0 +1,24 @@
+import onboarding1 from '@/assets/images/onboarding/onboarding1.png';
+import onboarding2 from '@/assets/images/onboarding/onboarding2.png';
+import onboarding3 from '@/assets/images/onboarding/onboarding3.png';
+
+export const onBoardingSlides = [
+  {
+    id: 1,
+    image: onboarding1,
+    title: `이 사진이 좋을까?\n저 사진이 좋을까?`,
+    subtitle: `혼자 고민하지 말고,\n뽀토픽에게 맡겨보세요!`,
+  },
+  {
+    id: 2,
+    image: onboarding2,
+    title: `고민되는 사진을 올리고,\n투표를 받아보세요!`,
+    subtitle: `사진을 올리면, 친구의 투표 시작!\n더 많은 표를 받을 사진은?`,
+  },
+  {
+    id: 3,
+    image: onboarding3,
+    title: `링크만 던지면,\n 베스트컷 결정!`,
+    subtitle: `링크 하나만 보내면\n누구나 쉽게 참여할 수 있어요!`,
+  },
+];
--- src/pages/OnBoarding/OnBoardingPage.tsx
+++ src/pages/OnBoarding/OnBoardingPage.tsx
@@ -1,19 +1,34 @@
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import LoginButton from '@/components/login/button/LoginButton';
+import { onBoardingSlides } from '@/constants/onboarding';
 
 export default function OnBoardingPage() {
   return (
-    <div className="flex justify-center w-full h-screen px-7 flex-col">
-      <div className="flex-1 flex items-center flex-col text-center">
-        <span className="flex items-center text-h1">
-          이 사진이 좋을까
-          <br />저 사진이 좋을까?
-        </span>
+    <div className="flex flex-col justify-between w-full h-screen px-7 relative">
+      <div className="flex-1 flex items-center">
+        <Swiper
+          modules={[Pagination]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          {onBoardingSlides.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div className="h-full flex flex-col items-center text-center">
+                <span className="text-h1 whitespace-pre-line mt-[160px] mb-[20px]">
+                  {slide.title}
+                </span>
 
-        <span className="flex items-center text-title-medium mt-5">
-          혼자 고민하지 말고,
-          <br />
-          뽀토픽에게 맡겨보세요!
-        </span>
+                <span className="text-title-medium whitespace-pre-line mb-[50px]">
+                  {slide.subtitle}
+                </span>
+                <img src={slide.image} className="w-[310px] mb-14" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="mb-16">
         <LoginButton />
